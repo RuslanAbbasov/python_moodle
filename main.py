8c06d3d640aae0d5932923814a6cc6db0e0b6bfe
Ruslan <ruslan.abbasov@urfu.me>
2022-11-24 00:27:54 +0500

    create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(1, 'https://github.com/RuslanAbbasov/python_moodle/blob/6620625fd023c521cca1d82941a8dc620324fe91/2.1.3/pdf_report.py')
+
+# import table_report
+import pdf_report
+
