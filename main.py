3e723506039997e4bac0e3d37877ad54d5dc40c1
Ruslan <ruslan.abbasov@urfu.me>
2022-11-24 23:52:22 +0500

    add doc

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 import pdf_report
 import table_report
 
-# SOME DIFF
-command = "".join(input(
-    'Введите, что вы желаете получить\n  -Вакансии- или -Статистика-\nПоле для ввода:'
-                       ).lower().split())
-print(command)
-if command == 'вакансии':
-    table_report.InputConnect()
-elif command == 'статистика':
-    pdf_report.InputConnect()
-else:
-    print('Вы ввели некорректные данные\nНеобходимо ввести\n-Вакансии- или -Статистика-')
+"""
+Файл main.py, реализующий выбор функциональности программы.
+
+Файд имеет метод main, который и реализует работу.
+"""
+
+
+def main():
+    """"
+    Вызываемый метод, который получает команду от пользователя (-Вакансии- или -Статистика-).
+
+    Ввод сразу опускает строку до нижнего регистра и исключает многочисленные пробелы
+    """
+    command = "".join(input(
+        'Введите, что вы желаете получить\n  -Вакансии- или -Статистика-\nПоле для ввода:'
+    ).lower().split())
+    print(command)
+    if command == 'вакансии':
+        table_report.InputConnect()
+    elif command == 'статистика':
+        pdf_report.InputConnect()
+    else:
+        print('Вы ввели некорректные данные\nНеобходимо ввести\n-Вакансии- или -Статистика-')
+
+
+if __name__ == '__main__':
+    import doctest
+
+    main()
+    doctest.testmod()
