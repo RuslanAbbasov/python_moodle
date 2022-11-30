01544fcf736d8bf94b7233d270a73a16890e38d1
Ruslan <ruslan.abbasov@urfu.me>
2022-11-30 23:03:06 +0500

    add doc

--- table_report.py
+++ table_report.py
@@ -7,6 +7,9 @@ from prettytable import PrettyTable
 
 
 class CommonTools:
+    """Класс с вспомогательными методами
+
+    """
     rus_names = {'Название': 'name', 'Описание': 'description', 'Навыки': 'key_skills', 'Опыт работы': 'experience_id',
                  'Премиум-вакансия': 'premium', 'Компания': 'employer_name', 'Оклад': 'salary_from',
                  'Верхняя граница вилки оклада': 'salary_to', 'Оклад указан до вычета налогов': 'salary_gross',
@@ -17,11 +20,20 @@ class CommonTools:
 
     @staticmethod
     def exit_with_print(line):
+        """Выход из программы после печати ошибки
+
+        :param line:
+        """
         print(line)
         exit()
 
     @staticmethod
     def edit_line(line):
+        """Фильтрация строки от html тегов и '\n', так же переводит 'True' и 'False'
+
+        :param line: (str)
+        :return: string (str)
+        """
         string = re.sub(r'<[^>]+>', '', line)
         if '\n' not in string:
             string = ' '.join(string.split())
@@ -31,11 +43,26 @@ class CommonTools:
 
 
 class Salary:
+    """Класс для представления зарплаты
+
+    Attribute:
+        salary_from(str): Нижняя граница вилки оклада
+        salary_to(str): Верхняя граница вилки оклада
+        salary_gross(str): Оклад указан до вычета налогов
+        salary_currency(str): Идентификатор валюты оклада
+    """
     currency_to_rub = {
         "Манаты": 35.68, "Белорусские рубли": 23.91, "Евро": 59.90, "Грузинский лари": 21.74, "Киргизский сом": 0.76,
         "Тенге": 0.13, "Рубли": 1, "Гривны": 1.64, "Доллары": 60.66, "Узбекский сум": 0.0055}
 
     def __init__(self, salary_from, salary_to, salary_gross, salary_currency):
+        """Инициализирует объект Salary
+
+        :param salary_from: (str): Нижняя граница вилки оклада
+        :param salary_to: (str): Верхняя граница вилки оклада
+        :param salary_gross: (str): Оклад указан до вычета налогов
+        :param salary_currency: (str): Идентификатор валюты оклада
+        """
         self.salary_from = salary_from
         self.salary_to = salary_to
         self.salary_gross = salary_gross
@@ -43,18 +70,44 @@ class Salary:
 
     @staticmethod
     def currency_translate(salary_from, salary_to, salary_currency):
+        """Переводит зарплату в рубли
+
+        :param salary_from: (str): Нижняя граница вилки оклада
+        :param salary_to: (str): Верхняя граница вилки оклада
+        :param salary_currency: (str): Идентификатор валюты оклада
+        :return:
+            salary_from(str): Нижняя граница вилки оклада
+            salary_to(str): Верхняя граница вилки оклада
+        """
         salary_from = int(math.trunc(float(salary_from))) * Salary.currency_to_rub[salary_currency]
         salary_to = int(math.trunc(float(salary_to))) * Salary.currency_to_rub[salary_currency]
         return salary_from, salary_to
 
 
 class Vacancy:
+    """Класс для представления вакансии
+
+    Attribute:
+        name(str): название вакансии
+        description(str): описание
+        key_skills(str): Навыки
+        experience_id(str): Опыт работы
+        premium(str): Премиум-вакансия
+        employer_name(str): Компания
+        salary(str): Зарплата
+        area_name(str): Местонахождение
+        published_at(str): Дата публикации
+    """
     experience_rus = {'noExperience': 'Нет опыта',
                       'between1And3': 'От 1 года до 3 лет',
                       'between3And6': 'От 3 до 6 лет',
                       'moreThan6': 'Более 6 лет'}
 
     def __init__(self, dictionary):
+        """ Инициализация объекта Vacancy
+
+        :param dictionary: (dict) словарь вакансии
+        """
         self.name = dictionary['name']
         self.description = dictionary['description']
         self.key_skills = dictionary['key_skills']
@@ -68,7 +121,17 @@ class Vacancy:
 
 
 class DataSet:
+    """Класс для составления списка вакансий
+
+    Attribute:
+        file_name(str): имя файла
+        vacancies_objects(list[Vacancy]): датасет вакансий
+    """
     def __init__(self, file_name):
+        """Инициализирует объект DataSet
+
+        :param file_name: (str): имя файла
+        """
         self.file_name = file_name
         data_tuple = DataSet.read_csv(file_name)
         resume_dict = DataSet.csv_filter(data_tuple[0], data_tuple[1])
@@ -79,6 +142,11 @@ class DataSet:
 
     @staticmethod
     def read_csv(file_name):
+        """парсит csv файл и проверяет ег на пустоту или отсутствие данных
+
+        :param file_name:(str): имя файла
+        :return: vacancies, columns (tuple[list]): кортеж из списка названий колонок и списка данных о вакансиях
+        """
         reader_csv = csv.reader(open(file_name, encoding='utf_8_sig'))
         list_data = [x for x in reader_csv]
         if len(list_data) == 0:
@@ -91,6 +159,12 @@ class DataSet:
 
     @staticmethod
     def csv_filter(reader, list_naming):
+        """Обрабатывает данные и преобразует их в список словарей
+
+        :param reader: (list) данные о вакансиях
+        :param list_naming: (list) название колонок
+        :return: resumes: (list[dict]) список словарей
+        """
         resumes = []
         sentences = {}
         for resume in reader:
@@ -102,13 +176,24 @@ class DataSet:
 
 
 class InputConnect:
+    """Класс для работы с входными данными
+
+    """
     def __init__(self):
+        """Реализует вызов методов чтения входных данных,
+         создания датасета вакансий и печати таблицы
+
+        """
         params = InputConnect.get_params()
         data_set = DataSet(params[0])
         InputConnect.print_vacancies(data_set, params[1], params[2], params[3], params[4], params[5])
 
     @staticmethod
     def get_params():
+        """Получение входных данных и работа с некорректными данными
+
+        :return: tuple[str] кортеж с входными данными
+        """
         file_name = input('Введите название файла: ')
         parameter = input('Введите параметр фильтрации: ')
         sort_parametr = input('Введите параметр сортировки: ')
@@ -133,6 +218,14 @@ class InputConnect:
 
     @staticmethod
     def salary_format(salary_from, salary_to, salary_gross, salary_currency):
+        """Объединение информации о зарплате в строку
+
+        :param salary_from:(str) Нижняя граница оклада
+        :param salary_to:(str) Верхняя граница оклада
+        :param salary_gross:(str) Оклад указан до вычета налогов
+        :param salary_currency:(str) Валюта
+        :return: (str) строка, содержащая всю информацию о зарплате
+        """
         currency = InputConnect.currency_rus[salary_currency]
         if salary_gross == 'Нет':
             is_gross = 'С вычетом налогов'
@@ -145,6 +238,11 @@ class InputConnect:
 
     @staticmethod
     def formatter(row):
+        """Форматирует информацию объекта Vacancy и создает из нее словарь
+
+        :param row:(Vacancy) объект класса (Vacancy)
+        :return:new_dict(dict[str, str]): словарь вакансии
+        """
         new_dict = {}
         dict_names = list(CommonTools.rus_names.values())
         dict_names = dict_names[:7] + dict_names[10:]
@@ -160,7 +258,18 @@ class InputConnect:
 
     @staticmethod
     def do_filter(data, filter_list):
+        """Фильтрация данных
+        
+        :param data: (dict) словарь с данными
+        :param filter_list: (list) список параметров фильтрации
+        :return: filtered_list (list) список фильтрованных знач
+        """
         def for_filter(row):
+            """Функция используется функцией do_filter для объекта filter
+
+            :param row:
+            :return:
+            """
             if filter_list == '':
                 return True
             parameter = filter_list.split(': ')
@@ -190,6 +299,13 @@ class InputConnect:
 
     @staticmethod
     def do_sort(data, sort, reverse):
+        """Реализует сортировку по определенному параметру
+
+        :param data: (dict) список с данными
+        :param sort: (str) параметр сортировки
+        :param reverse: (bool) переворот список
+        :return: data: (dict) сортированный список с данными
+        """
         is_reverse = False
         if reverse == 'Да':
             is_reverse = True
@@ -197,6 +313,11 @@ class InputConnect:
         experience_sort = {'Нет опыта': 0, 'От 1 года до 3 лет': 1, 'От 3 до 6 лет': 2, 'Более 6 лет': 3}
 
         def for_sort(row):
+            """Функция используется функцией do_sort() для реализации сортировки
+
+            :param row: (dict): Словарь с данными
+            :return: (int or str or datetime): Значение для сортировки
+            """
             if sort == 'Оклад':
                 salary = row['salary_from'].split()
                 currency = re.search(r'\((.*?)\)', row['salary_from']).group(1)
@@ -219,6 +340,14 @@ class InputConnect:
 
     @staticmethod
     def create_data(data, filter_list, sort, reverse):
+        """Создает итоговый список данных с итоговыми правками для печати
+
+        :param data: (DataSet): Набор данных
+        :param filter_list: (list): Список с данными для фильтрации
+        :param sort: (str): Параметр сортировки
+        :param reverse: (bool) переворот список
+        :return: sorted_list: (list): итоговый список с данными
+        """
         result_list = []
         for i in range(len(data.vacancies_objects)):
             dictionary = InputConnect.formatter(data.vacancies_objects[i])
@@ -247,6 +376,15 @@ class InputConnect:
 
     @staticmethod
     def print_vacancies(data_set, filter_list, sort, reverse, indexes, fields_list):
+        """Печать данных в таблице
+
+        :param data_set: (DataSet): Набор данных
+        :param filter_list: (list): Список с данными для фильтрации
+        :param sort: (str): Параметр сортировки
+        :param reverse: (bool) переворот список
+        :param indexes: (list): Диапазон вывода строк
+        :param fields_list:(list): Список колонок, которые нужно вывести
+        """
         table = PrettyTable()
         rus_list = list(CommonTools.rus_names.keys())
         table.field_names = ['№'] + rus_list[:7] + rus_list[10:]
